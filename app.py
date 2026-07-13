8dda7ab422839ca69e9cddebb928617f004aabc4
kush2501 <coder8342@gmail.com>
2026-07-13 14:07:27 +0530

    Add Sequential workflow

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ def sub(a, b):
 def mul(a, b):
     return a*b
 
+
 if "__name__" == "__main__":
 
     result1 = add(4, 5)
     result2 = sub(4, 1)
-    result3 = mul(6,7)
+    result3 = mul(6, 7)
     print(result1, result2, result3)
